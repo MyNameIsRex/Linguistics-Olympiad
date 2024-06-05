cc702c0b7f932c5ab4148de98ea3192f1b0520de
MyNameIsRex <rex52836@gmail.com>
2024-06-05 19:33:56 +0800

    ✂️ Syllable split in progress

--- data/isbukunbunun/parser/Parser.java
+++ data/isbukunbunun/parser/Parser.java
@@ -1,17 +1,55 @@
 package data.isbukunbunun.parser;
 
+import java.util.List;
+import java.util.ArrayList;
+
 public class Parser 
 {
     //IPA Vowels
-    public static final char[] IPA_VOWELS = {'i', 'y', 'ɨ', 'ʉ', 'ɯ', 'u', 'ɪ', 'ʏ', 'ʊ', 'e', 'ø', 'ɘ', 'ɵ', 'ɤ', 'o', 'ə', 'ɛ', 'œ', 'ɜ', 'ɞ', 'ʌ', 'ɔ', 'ɐ', 'æ', 'a', 'ɶ', 'ɑ', 'ɒ'};
+    public static final List<Character> IPA_VOWELS = new ArrayList<>();
     
     //Global Variables
     private String input;
     private String output = "";
+
+    private List<String> syllables;
     
     public void setup(String input)
     {
         this.input = input + "-()";
+        this.syllables = new ArrayList<>(){};
+        
+        char[] vowelArray = {'i', 'y', 'ɨ', 'ʉ', 'ɯ', 'u', 'ɪ', 'ʏ', 'ʊ', 'e', 'ø', 'ɘ', 'ɵ', 'ɤ', 'o', 'ə', 'ɛ', 'œ', 'ɜ', 'ɞ', 'ʌ', 'ɔ', 'ɐ', 'æ', 'a', 'ɶ', 'ɑ', 'ɒ'};
+       
+        for (char vowel : vowelArray)
+            IPA_VOWELS.add(vowel);
+
+
+        StringBuilder syllable = new StringBuilder();
+
+        for (int index = 0; index < this.input.length(); index++)
+        {
+            if (IPA_VOWELS.contains(this.input.charAt(index)))
+            {
+                if (this.input.charAt(index - 1) == 'j')
+                    syllable.append(this.input.charAt(index - 2));
+                
+                syllable.append(this.input.charAt(index - 1)).append(this.input.charAt(index));
+
+                if (this.input.charAt(index + 1) == ':' || (!IPA_VOWELS.contains(this.input.charAt(index + 2)) && this.input.charAt(index + 1) != '-'))
+                    syllable.append(this.input.charAt(index + 1));
+
+                if ((this.input.charAt(index + 1) == 'j' || this.input.charAt(index + 1) == 'w') && this.input.charAt(index + 2) != '-')
+                    syllable.append(this.input.charAt(index + 2));
+
+                this.syllables.add(syllable.toString());
+                System.out.println(syllable);
+                syllable.setLength(0);
+            }
+
+            if (this.input.charAt(index) == '-' && this.input.charAt(index + 1) == '(')
+                break;
+        }
     }
 
     public void parse()
